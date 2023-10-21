2c5e96fdccbf7faaf3c11166f4e1b6f7adeaf31a
Zach Bauer <azbauer8@gmail.com>
2023-10-21 18:02:31 -0400

    added dynamic date range to query

--- src/Games.tsx
+++ src/Games.tsx
@@ -4,10 +4,28 @@ import axios from "axios";
 function Games() {
     const [games, setGames] = useState([]);
     const [searchQuery, setSearchQuery] = useState([]);
+
+    const dateRange = 30;
+
+    const currentDate = new Date();
+    const dateStart = new Date(currentDate);
+    dateStart.setDate(currentDate.getDate() - dateRange);
+    let year = dateStart.getFullYear();
+    let month = String(dateStart.getMonth() + 1).padStart(2, "0");
+    let day = String(dateStart.getDate()).padStart(2, "0");
+    const formattedDateStart = `${year}-${month}-${day}`;
+
+    const dateEnd = new Date(currentDate);
+    dateEnd.setDate(currentDate.getDate() + dateRange);
+    year = dateEnd.getFullYear();
+    month = String(dateEnd.getMonth() + 1).padStart(2, "0");
+    day = String(dateEnd.getDate()).padStart(2, "0");
+    const formattedDateEnd = `${year}-${month}-${day}`;
+
     useEffect(() => {
         axios
             .get(
-                `https://api.rawg.io/api/games?dates=2023-01-01,2023-12-31&ordering=-metacritic&key=${
+                `https://api.rawg.io/api/games?dates=${formattedDateStart},${formattedDateEnd}&ordering=-metacritic&page_size=100&key=${
                     import.meta.env.VITE_RAWG_API_KEY
                 }`
             )
